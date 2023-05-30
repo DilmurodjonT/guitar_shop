79f6849906bb1394eeca16e01685d8d2f247faf9
DilmurodjonT <dilmurodjontadjibaev@gmail.com>
2023-05-30 17:05:45 +0500

    end catalog

---
+++ constants/catalog.js
@@ -0,0 +1,64 @@
+const CATALOG = [
+  {
+    id: "el1",
+    name: "FENDER SQUIER BULLET STRAT HT HSS BLK",
+    img: "images/1.jpg",
+    price: 13600,
+  },
+
+  {
+    id: "el2",
+    name: "FENDER SQUIER AFFINITY",
+    img: "images/2.jpg",
+    price: 23900,
+  },
+
+  {
+    id: "el3",
+    name: "IBANEZ GIO GRG121DX-BKF BLACK FLAT",
+    img: "images/3.jpg",
+    price: 21600,
+  },
+  {
+    id: "el4",
+    name: "EPIPHONE LES PAUL STUDIO LT EBONY",
+    img: "images/4.jpg",
+    price: 19900,
+  },
+  {
+    id: "el5",
+    name: "FENDER SQUIER BULLET TREM HSS AWT",
+    img: "images/5.jpg",
+    price: 14500,
+  },
+  {
+    id: "el6",
+    name: "FENDER SQUIER BULLET MUSTANG HH BLK",
+    img: "images/6.jpg",
+    price: 14600,
+  },
+  {
+    id: "el7",
+    name: "FENDER SQUIER BULLET STRAT HT AWT",
+    img: "images/7.jpg",
+    price: 13600,
+  },
+  {
+    id: "el8",
+    name: "JACKSON JS22-7 DINKY SATIN BLACK",
+    img: "images/8.jpg",
+    price: 28700,
+  },
+  {
+    id: "el9",
+    name: "JACKSON JS11 DINKY OLYMPIC WHITE",
+    img: "images/9.jpg",
+    price: 18100,
+  },
+  {
+    id: "el10",
+    name: "YAMAHA PACIFICA 012 WH",
+    img: "images/10.jpg",
+    price: 15900,
+  },
+];
